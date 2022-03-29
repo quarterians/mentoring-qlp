897f0a4b1d88891b764f6fe1d8c745fefc3d3968
Jem Angkasa <jem.angkasa91@gmail.com>
2022-03-29 21:19:27 +0700

    coba github to hosting

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -7,13 +7,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Mentoring QLP</title>
+    <title>Mentoring QLP [Coba]</title>
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/jquery.qeditor.js') }}" type="text/javascript"></script>
-    <script src="https://kit.fontawesome.com/e5d58c1a7c.js" crossorigin="anonymous"></script>    
+    <script src="https://kit.fontawesome.com/e5d58c1a7c.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -28,22 +28,22 @@
         main{
             height: 90vh;
         }
-        footer {            
+        footer {
             bottom:0;
             width:100%;
             height:80px;
             background:#2A5F72;
             text-align: center;
-            color: white;            
-            font-weight: 500;                   
-        }    
+            color: white;
+            font-weight: 500;
+        }
     </style>
 </head>
 <body>
     <div id="app">
         <main class="pb-sm-5 py-lg-5">
             <div class="jumbotron">
-                <div class="container px-3 mx-auto row">        
+                <div class="container px-3 mx-auto row">
                     <div class="col-lg-6"  style="padding-top: 5%">
                         <h1 class="font-weight-bolder" id="jumbotron-heading">Quarters Life Projects is Under Maintenance!</h1>
                         <p style="font-size: 18px text-justify">Layanan 1-on-1 consulting dengan mentor sedang ditutup. <br>Tunggu re-launch kami di Bulan Maret 2022 ya. <br>See you..<br>By: Quarter Life Projects Team</p>
@@ -54,10 +54,10 @@
                     </div>
                 </div>
             </div>
-        </main>        
+        </main>
         <footer class="py-3">
             <p><b>QUARTER LIFE PROJECTS TEAM</b><br>Copyright © 2021 Quarter Life Projects</p>
         </footer>
-    </div> 
+    </div>
 </body>
 </html>
