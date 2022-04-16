c7c3f4af66743e9243cecc72fca8ea041d9eeb33
Yosua Sandi Susanto <yosuasandi.ys@gmail.com>
2022-04-16 11:24:33 +0700

    bug css

--- resources/views/qlp/searchmentor.blade.php
+++ resources/views/qlp/searchmentor.blade.php
@@ -2,105 +2,113 @@
 @section('content')
     <section class="text-dark bg-bubble text-center mentor-section">
         <div class="container">
-            <h1 class="text-leading mb-2">Temukan mentormu sekarang!</h1>
+            <h1 class="font-weight-semi mb-3">Temukan mentormu sekarang!</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia reprehenderit laudantium <br/> aliquam iure aperiam minima at non doloribus, ipsum, dolor, nulla alias<br/> omnis? Hic quis optio minima aliquid assumenda aspernatur.</p>
-            <div class="text-leading1 fw-semibold mt-5">
+            <div class="text-24 fw-semibold mt-5">
                 Bingung cara booking mentor? <a href="" class="text-orange">klik disini</a>
             </div>
         </div>
     </section>
-    <form action="" id="form-filtering">
-        <section class="search py-4 filter">
-            <div class="container">
-                <div class="flex-search align-items-center">
-                    <div class="text-24 fw-semibold">Cari Mentor :</div>
-                    <div class="ms-4 search-mentor-width">
-                        <div class="box-search flex align-items-center">
-                            <div class="">
-                                <i class="text-16 fa fa-search"></i>
-                            </div>
-                            <form action="/searchmentor" method="GET">
-                                <div class="ms-2 w-100">
-                                    <input type="text" class="form" placeholder="Search" name=search>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="text-24 ms-5 fw-semibold search-all-width">Cari Semua Mentor :</div>
-                    <div class="">
-                        <button class="btn shadow-sm fw-semibold" onchange="sorting()">Cari Semua</button>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="flex align-items-center">
-                    <div class="">
-                        <div class="text-24 fw-semibold">Filter Mentor:</div>
-                    </div>
-                    <div class="w-15vw ms-3">
-                        <select class="select-filter" id="kategori" name="kategori">
-                            <option value="">Semua Kategori</option>
-                            @isset($pillars)
-                            @foreach ($pillars->unique('id') as $pillar)
-                            <option value="{{$pillar->id}}">{{$pillar->pillar}}</option>
-                            @endforeach
-                            @endisset
-                        </select>
-                    </div>
-                    <div class="w-15vw ms-3">
-                        <select class="select-filter" id="subkategori" name="sub_kategori">
-                            <option value="">Semua Sub Kategori</option>
-                            @isset($subcategories)
-                            @foreach ($subcategories->unique('id') as $expertise)
-                            <option value="{{$expertise->id}}">{{$expertise->expertise}}</option>
-                            @endforeach
-                            @endisset
-                        </select>
-                    </div>
-                    <div class="w-15vw ms-3">
-                        <select class="select-filter" id="jurusan" name="jurusan">
-                            <option value="">Semua Jurusan</option>
-                            @isset($jurusan)
-                            @foreach ($jurusan->unique('id') as $jurusan)
-                            <option value="{{$jurusan->id}}">{{$jurusan->jurusan}}</option>
-                            @endforeach
-                            @endisset
-                        </select>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <script>
-            function sorting()
-            {
-                document.querySelector('#form-filtering').submit();
-            }
-        </script>
-    </form>
-    <section class="mentor-list py-8">
+    <section id="filter_searchmentor">
         <div class="container">
-            <div class="grid-4">
-                @foreach($user as $key => $user)
-                <div class="card text-center">
-                    <div class="card-body">
-                        <img src="{{ 'img/uploads/'.$user->profile_picture  }}" alt="" width="300" class="">
-                        <div class="mt-4">
-                            <div class="fw-bold text-24">{{ $user-> name}}</div>
-                            @foreach($user->expertise as $expertise)
-                            <div class="fw-normal text-20 mt-1">{{ $expertise-> expertise }} </div>
-                            @endforeach
-                            <div class="mt-3">
-                                <small>Total Session</small>
+            <form action="" id="form-filtering" class="p-0" style="background-color:#F5F5F5">
+                <section class="search filter">
+                    <div class="flex-search align-items-center mb-3">
+                        <div class="w-10vw text-18 font-weight-semi">Cari Mentor :</div>
+                        <div class="ms-3 search-mentor-width">
+                            <div class="box-search flex align-items-center">
+                                <div class="">
+                                    <i class="text-16 fa fa-search"></i>
+                                </div>
+                                <form action="/searchmentor" method="GET">
+                                    <div class="ms-2 w-100">
+                                        <input type="text" class="form" placeholder="Search" name=search>
+                                    </div>
+                                </form>
                             </div>
-                            <div class="fw-normal text-20 mt-1">{{ $user->session_hour}}</div>
-                            <div class="" style="margin-top: 5em;">
-                                <a href="" class="btn text-16 fw-normal">View Profile</a>
+                        </div>
+                        <div class="text-18 ms-5 font-weight-semi search-all-width">Cari Semua Mentor :</div>
+                        <div class="">
+                            <button class="btn btn-primary shadow-sm fw-semibold" onchange="sorting()">Cari Semua</button>
+                        </div>
+                    </div>
+                    <div class="flex align-items-center">
+                        <div class="">
+                            <div class="w-10vw text-18 font-weight-semi">Filter Mentor :</div>
+                        </div>
+                        <div class="w-15vw ms-3">
+                            <select class="select-filter" id="kategori" name="kategori">
+                                <option value="">Semua Kategori</option>
+                                @isset($pillars)
+                                @foreach ($pillars->unique('id') as $pillar)
+                                <option value="{{$pillar->id}}">{{$pillar->pillar}}</option>
+                                @endforeach
+                                @endisset
+                            </select>
+                        </div>
+                        <div class="w-15vw ms-3">
+                            <select class="select-filter" id="subkategori" name="sub_kategori">
+                                <option value="">Semua Sub Kategori</option>
+                                @isset($subcategories)
+                                @foreach ($subcategories->unique('id') as $expertise)
+                                <option value="{{$expertise->id}}">{{$expertise->expertise}}</option>
+                                @endforeach
+                                @endisset
+                            </select>
+                        </div>
+                        <div class="w-15vw ms-3">
+                            <select class="select-filter" id="jurusan" name="jurusan">
+                                <option value="">Semua Jurusan</option>
+                                @isset($jurusan)
+                                @foreach ($jurusan->unique('id') as $jurusan)
+                                <option value="{{$jurusan->id}}">{{$jurusan->jurusan}}</option>
+                                @endforeach
+                                @endisset
+                            </select>
+                        </div>
+                    </div>
+                </section>
+        
+                <script>
+                    function sorting()
+                    {
+                        document.querySelector('#form-filtering').submit();
+                    }
+                </script>
+            </form>
+        </div>
+    </section>
+    <section id="list_mentor">
+        <div class="container">
+            <div class="mentor-list py-8">
+                <div class="grid-4">
+                    @foreach($user as $key => $user)
+                    <div class="card text-center py-3" style="width: auto;">
+                        <div class="card-body" style="align-items: center ;">
+                        <div class="image">
+                            @if($user->profile_picture == NULL)
+                            <img src="{{ asset('/qlp/img/qlp_logo.png') }}" alt="">
+                            @else
+                            <img src="{{ asset('img/uploads/'.$user->profile_picture) }}" alt="">
+                            @endif
+                        </div>
+                            <div class="mt-4">
+                                <div class="h-10vw fw-bold text-18">{{ $user-> name}}</div>
+                                @foreach($user->expertise as $expertise)
+                                <div class="fw-normal text-18 mt-1">{{ $expertise-> expertise }} </div>
+                                @endforeach
+                                <div class="mt-3">
+                                    <small>Total Session</small>
+                                </div>
+                                <div class="fw-normal text-20 mt-1">{{ $user->session_hour}} Hour</div>
+                                <div class="" style="margin-top: 5em;">
+                                    <a href="" class="btn btn-primary text-16 fw-normal">View Profile</a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
         </div>
     </section>
