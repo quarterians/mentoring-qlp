417496ee7d967832cadac5840a3fe69df4205eb7
Mochammad Itmamul Wafa <itmamulwafa11@yahoo.com>
2022-06-14 18:31:25 +0700

    Perbaiki header and footer
    1. ubah logo
    2. benerin tombol navbar, code<header></header> tak pindah ke navbar file.
    3. mengubah footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,40 +10,25 @@
 
     <title>Mentoring QLP</title>
     {{-- Bootstrap --}}
-    <link
-      href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap"
-      rel="stylesheet"
-    />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-    crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-      integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/swiper/swiper-bundle.min.css"
-    />
-    
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
     <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
-    
+
 
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="{{ asset('/js/app.js') }}" defer></script>
     <script src="{{ asset('/js/jquery.qeditor.js') }}" type="text/javascript"></script>
-    <script src="https://kit.fontawesome.com/e5d58c1a7c.js" crossorigin="anonymous"></script>    
+    <script src="https://kit.fontawesome.com/e5d58c1a7c.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -58,72 +44,21 @@
     <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
 
 </head>
+
 <body>
     <div id="app">
-        <header class="bg-white header">
-            <div class="container">
-                <div class="flex justify-between align-items-center">
-                    <div class="flex align-items-center">
-                        <!-- <div class="logo">
-                            <img src="{{ asset('/qlp/img/qlp_logo.png') }}" alt="" class="">
-                        </div> -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            <img src="{{ asset('/qlp/img/qlp_logo.png') }}" alt="QLP Logo">
-                        </a>
-                        <div class="ms-3">
-                            <ul class="nav">
-                                <li class="nav-item">
-                                    <a href="{{ url('/dev') }}" class="text-dark fw-semibold active">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="" class="text-dark fw-semibold">About Us <i class="fas ms-1 fa-caret-down"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="" class="text-dark fw-semibold">Program</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="" class="text-dark fw-semibold">Guide <i class="fas ms-1 fa-caret-down"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="{{ url('/dev/FAQ') }}" class="text-dark fw-semibold">FAQ</a>
-                                </li>
-                                @auth      
-                                <li class="nav-item nav-item-mobile">
-                                    <a class="nav-link font-weight-bold" href="{{ route('setting.profile') }}">Edit Profil</a>
-                                </li>    
-                                <li class="nav-item nav-item-mobile">
-                                    <a class="nav-link font-weight-bold" href="{{ route('setting.account') }}">Edit Akun</a>
-                                </li>                                                                    
-                                @endauth
-                            </ul>
-                        </div>
-                    </div>
-                    @guest
-                    <div class="mt-sm-2 pb-sm-2 w-sm-100">
-                        <a href="{{ route('login') }}" class="btn btn-primary shadow-sm text-center fw-semibold w-sm-100 align-items-end">Menjadi Mentor</a>
-                    </div>
-                    @else
-                    <div class="mt-sm-2 pb-sm-2 w-sm-100">
-                        <a href="{{ route('logout') }}" class="btn shadow-sm text-center fw-semibold w-sm-100" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a>
-                    </div>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form> 
-                    @endguest
-                </div>
-            </div>
-        </header>
+        @include('qlp.navbar')
 
         @if (Request::is('mentors/*') || Request::is('home') || Request::is('login') || Request::is('register'))
-            <main>    
-        @else
+        <main>
+            @else
             <main class="py-4" style="padding-top: 0 !important; padding-bottom: 0 !important">
-        @endif                
-            @yield('content')
-        </main>        
+                @endif
+                @yield('content')
+            </main>
 
-        @include('qlp.footer')
-        <!-- <footer>
+            @include('qlp.footer')
+            <!-- <footer>
 
             {{-- <p><b>QUARTER LIFE PROJECTS TEAM</b><br>Copyright © 2021 Quarter Life Projects</p> --}}
 
@@ -243,39 +178,42 @@
                         </small>
                     </div>
                 </div>
-               
             </div>
 
         </footer> -->
-    </div>    
+    </div>
 </body>
+
 </html>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript">
-  const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+    const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-  accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", event => {
-      
-      // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-      
-      // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-      // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-      //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-      //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-      // }
+    accordionItemHeaders.forEach(accordionItemHeader => {
+        accordionItemHeader.addEventListener("click", event => {
 
-      accordionItemHeader.classList.toggle("active");
-      const accordionItemBody = accordionItemHeader.nextElementSibling;
-      if(accordionItemHeader.classList.contains("active")) {
-        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-      }
-      else {
-        accordionItemBody.style.maxHeight = 0;
-      }
-      
+            // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+            // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+            // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+            //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+            //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+            // }
+
+            accordionItemHeader.classList.toggle("active");
+            const accordionItemBody = accordionItemHeader.nextElementSibling;
+            if (accordionItemHeader.classList.contains("active")) {
+                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+            } else {
+                accordionItemBody.style.maxHeight = 0;
+            }
+
+        });
     });
-  });
+
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
 @yield('js')
--- resources/views/qlp/footer.blade.php
+++ resources/views/qlp/footer.blade.php
@@ -1,31 +1,29 @@
 <footer class="footer text-white py-9">
         <div class="container">
             <div class="flex justify-between">
-                <div class="w-30 mb-3">
-                    <img src="{{ asset('/qlp/img/logo-full.png') }}" alt="">
-                    <p class="text-semi">
+                <div class="mb-3 w-30 text-break lh-sm mb-1 ">
+                    <img class="mb-3" src="{{ asset('/qlp/img/logo/logo-QLP.png') }}" height="75px" width="auto" alt="Max-width 100%">
+                    <p class="text-semi text-justify mt-1">
                         Platform sosial yang didirikan pada 27 Maret 2021 dengan misi untuk menjadi sistem pendukung bagi generasi Z selama masa
                         Quarter Life Crisis mereka.
                     </p>
                 </div>
                 <div class="mb-3">
                     <div class="fw-bold text-16">
-                        About Us
+                        Tentang Kami
                     </div>
                     <ul class="list-nostyle mt-3 pl-0">
-                        <li><a href="" class="text-semi text-16">Visi</a></li>
-                        <li><a href="" class="text-semi text-16">Misi</a></li>
-                        <li><a href="" class="text-semi text-16">QLP Pilar</a></li>
-                        <li><a href="" class="text-semi text-16">Meet Our Team</a></li>
+                        <li><a href="" class="text-semi text-16">Visi & Misi</a></li>
                     </ul>
                 </div>
                 <div class="mb-3">
                     <div class="fw-bold text-16">
-                        Program
+                        Layanan
                     </div>
                     <ul class="list-nostyle mt-3 pl-0">
-                        <li><a href="" class="text-semi text-16">Weekly Webinar</a></li>
-                        <li><a href="" class="text-semi text-16">Quarterlife Mentoring</a></li>
+                        <li><a href="" class="text-semi text-16">Konsultasi</a></li>
+                        <li><a href="" class="text-semi text-16">Webinar</a></li>
+                        <li><a href="" class="text-semi text-16">Diaries</a></li>
                     </ul>
                 </div>
                 <div class="mb-3">
@@ -41,30 +39,32 @@
                         Bantuan
                     </div>
                     <ul class="list-nostyle mt-3 pl-0">
+                        <li><a href="" class="text-semi text-16">Panduan Konsultasi</a></li>
                         <li><a href="" class="text-semi text-16">FAQ</a></li>
-                        <li><a href="" class="text-semi text-16">Guide</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="mt-4">
-                <div class="fw-bold text-16">
-                    Email
+            <div class="d-flex justify-content-between">
+                <div class="mt-6">
+                    <div class="fw-bold text-16">
+                        Email
+                    </div>
+                    <p class="text-semi mt-1">quarterlife.projects@gmail.com</p>
                 </div>
-                <p class="text-semi mt-1">quarterlife.projects@gmail.com</p>
-            </div>
-            <div class="mt-6">
-                <div class="fw-bold text-16">
-                    Contact Us
+                <div class="mt-6 text-end">
+                    <div class="fw-bold text-16">
+                        Contact Us
+                    </div>
+                    <ul class="list-horizontal mt-2 pl-0">
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-youtube"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-spotify"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-telegram"></i></a></li>
+                        <li><a href="" class="text-green bg-white icon-sosmed"><i class="fa fa-envelope"></i></a></li>
+                    </ul>
                 </div>
-                <ul class="list-horizontal mt-2 pl-0">
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-instagram"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-youtube"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-spotify"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-linkedin"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fab fa-telegram"></i></a></li>
-                    <li><a href="" class="text-green bg-white icon-sosmed"><i class="fa fa-envelope"></i></a></li>
-                </ul>
             </div>
         </div>
     </footer>
